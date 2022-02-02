0fb32ce37be4eef16e4085dc46305056e78a8a17
eugene <volkoedov@gmail.com>
2022-02-02 06:53:47 +0300

    Sobar fix

--- src/main/java/vea/home/microservice/exceptions/GenericException.java
+++ src/main/java/vea/home/microservice/exceptions/GenericException.java
@@ -1,21 +1,23 @@
 package vea.home.microservice.exceptions;
 
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 abstract class GenericException extends RuntimeException {
-    private final Map<String, Object> details;
+    private final Map<String, Object> details=new HashMap<>();
     private final Integer code;
 
 
     protected GenericException(Integer code,String message, Map<String, Object> details) {
         super(message);
         this.code = code;
-        this.details = details;
+        this.details.putAll(details);
     }
 
     public Map<String, Object> getDetails() {
-        return details;
+        return Collections.unmodifiableMap(details);
     }
 
     public Integer getCode() {
