eeb2bd5935110526b0e61f62a649982a7456e902
volko <volkoedov@gmail.com>
2022-02-02 06:40:56 +0300

    Exceptions handler

--- src/main/java/vea/home/microservice/exceptions/CustomizedResponseExceptionHandler.java
+++ src/main/java/vea/home/microservice/exceptions/CustomizedResponseExceptionHandler.java
@@ -1,7 +1,6 @@
 package vea.home.microservice.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
