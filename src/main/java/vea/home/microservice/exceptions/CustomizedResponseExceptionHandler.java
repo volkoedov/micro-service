8ef954baf107222030965198f790f097719b5e66
volko <volkoedov@gmail.com>
2022-02-11 07:28:53 +0300

    Config validation exception handling

--- src/main/java/vea/home/microservice/exceptions/CustomizedResponseExceptionHandler.java
+++ src/main/java/vea/home/microservice/exceptions/CustomizedResponseExceptionHandler.java
@@ -1,8 +1,11 @@
 package vea.home.microservice.exceptions;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,23 +13,39 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 
-@ControllerAdvice
-@RestController
+@ControllerAdvice(annotations = {RestController.class})
 @Slf4j
 class CustomizedResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+        log.error("Unexpected exception", ex);
         ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), null, "Server error", 500, request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @ExceptionHandler(GenericException.class)
     public final ResponseEntity<Object> handleUSerNotFoundException(GenericException ex, WebRequest request) {
-        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), ex.getDetails(), ex.getMessage(), ex.getCode(),request.getDescription(false));
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), ex.getDetails(), ex.getMessage(), ex.getCode(), request.getDescription(false));
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, Object> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ExceptionResponse response = new ExceptionResponse(LocalDateTime.now(), errors, "Validation error", HttpStatus.BAD_REQUEST.value(), request.getDescription(false));
+
+        return ResponseEntity.badRequest().body(response);
+
+    }
 }
