bc0760fd6f4513b1008735252f09c8ca2f090754
eugene <volkoedov@gmail.com>
2022-02-18 07:26:04 +0300

    Static filtering by field annotation collection test

--- src/main/java/vea/home/microservice/controllers/FilteringController.java
+++ src/main/java/vea/home/microservice/controllers/FilteringController.java
@@ -4,11 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vea.home.microservice.services.SomeBeanDTO;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class FilteringController {
 
     @GetMapping("/filtering")
     public SomeBeanDTO retrieveSomeBean() {
         return new SomeBeanDTO("value1", "value2", "value3");
+    }
+
+    @GetMapping("/filtering-list")
+    public List<SomeBeanDTO> retrieveListOfSomeBean() {
+        return Arrays.asList(new SomeBeanDTO("value1", "value2", "value3"),new SomeBeanDTO("value12", "value22", "value32"));
     }
 }
--- src/test/java/vea/home/microservice/controllers/FilteringControllerTest.java
+++ src/test/java/vea/home/microservice/controllers/FilteringControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(controllers = {FilteringController.class})
@@ -21,6 +22,18 @@ class FilteringControllerTest {
                 .andExpect(jsonPath("$.field1").value("value1"))
                 .andExpect(jsonPath("$.field2").value("value2"))
                 .andExpect(jsonPath("$.field3").doesNotExist());
+
+    }
+
+    @Test
+    void successFilteringGetListTest() throws Exception {
+
+        mockMvc.perform(get("/filtering-list"))
+                .andDo(print())
+                .andExpect(jsonPath("[?(@.field1=='%s' && @.field2=='%s')]", "value1", "value2").exists())
+                .andExpect(jsonPath("[?(@.field1=='%s' && @.field2=='%s' && @.field3=='%s')]", "value1", "value2", "value3").doesNotExist())
+                .andExpect(jsonPath("[?(@.field1=='%s' && @.field2=='%s')]", "value12", "value22").exists())
+                .andExpect(jsonPath("[?(@.field1=='%s' && @.field2=='%s' && @.field3=='%s')]", "value12", "value22", "value32").doesNotExist());
 
     }
 }
