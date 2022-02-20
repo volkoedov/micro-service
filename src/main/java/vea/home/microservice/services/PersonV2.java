c7e8d8e7fd784ad06a7f1e81444a4a00f6fe6de7
volko <volkoedov@gmail.com>
2022-02-20 19:36:15 +0300

    Versioning by URI

---
+++ src/main/java/vea/home/microservice/services/PersonV2.java
@@ -0,0 +1,8 @@
+package vea.home.microservice.services;
+
+import lombok.Value;
+
+@Value
+public class PersonV2 {
+    Name name;
+}
---
+++ src/test/java/vea/home/microservice/controllers/PersonVersioningControllerTest.java
@@ -0,0 +1,33 @@
+package vea.home.microservice.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@WebMvcTest(controllers = {PersonVersioningController.class})
+class PersonVersioningControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void getPersonV1() throws Exception {
+        mockMvc.perform(get("/v1/person"))
+                .andDo(print())
+                .andExpect(jsonPath("$.name").value("Eugen Volkoedov"));
+    }
+
+    @Test
+    void getPersonV2() throws Exception {
+        mockMvc.perform(get("/v2/person"))
+                .andDo(print())
+                .andExpect(jsonPath("$.name.firstName").value("Eugen"))
+                .andExpect(jsonPath("$.name.lastName").value("Volkoedov"));
+    }
+}
